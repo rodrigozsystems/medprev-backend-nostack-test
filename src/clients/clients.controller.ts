f278c8948ca2238d9fb5b5956afa6cabf0ddc588
Rodrigo Henrique <rodrigo@zsystems.com.br>
2021-10-13 03:01:16 -0300

    Swagger

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,13 +1,17 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ClientsService } from './clients.service';
 import { CreateClientDTO } from './dtos/create-client.dto';
 import { IReturnClientDTO } from './dtos/return-client.dto';
 
+@ApiTags('clients')
 @Controller('clients')
 export class ClientsController {
   constructor(private clientsService: ClientsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create client' })
+  @ApiResponse({ status: 200, description: 'Cliente cadastrado com sucesso' })
   async createClient(
     @Body(ValidationPipe) createClientDto: CreateClientDTO,
   ): Promise<IReturnClientDTO> {
--- src/clients/dtos/create-client.dto.ts
+++ src/clients/dtos/create-client.dto.ts
@@ -1,22 +1,61 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
 import { Address } from 'src/addresses/address.entity';
 import { CreateAddressDTO } from 'src/addresses/dtos/create-address.dto';
 
 export class CreateClientDTO {
   @IsNotEmpty({ message: 'Informe o nome do cliente' })
+  @ApiProperty({ example: 'Rodrigo', description: 'Nome do Cliente' })
   name: string;
+  @ApiProperty({
+    example: 'Razão Social',
+    description: 'Razão Social do Cliente',
+    required: false,
+  })
   companyName?: string;
   @IsOptional()
   @IsEmail({}, { message: 'Informe um endereço de email válido' })
+  @ApiProperty({
+    example: 'cliente@email.com.br',
+    description: 'E-mail do Cliente',
+    required: false,
+  })
   email?: string;
   clientType: number;
   @IsNotEmpty({ message: 'Informe o documento do cliente' })
+  @ApiProperty({ example: '11111111111', description: 'Documento do Cliente' })
   nationalRegistration: string;
+  @ApiProperty({
+    example: 'F',
+    description: 'Sexo do Cliente',
+    required: false,
+  })
   gender?: string;
+  @ApiProperty({
+    example: '1989-12-06',
+    description: 'Data de Nascimento do Cliente',
+    required: false,
+  })
   birthDate?: string;
+  @ApiProperty({
+    example: '1181918195',
+    description: 'Telefone do Cliente',
+    required: false,
+  })
   phone?: string;
+  @ApiProperty({
+    example: '11981918195',
+    description: 'Celular do Cliente',
+    required: false,
+  })
   cellphone?: string;
+  @ApiProperty({
+    example: 'https://url.da.foto',
+    description: 'Foto do Cliente',
+    required: false,
+  })
   photo?: string;
   @IsNotEmpty({ message: 'Informe o endereço do cliente' })
+  @ApiProperty({ example: {}, description: 'Endereço do Cliente' })
   address: Address | CreateAddressDTO;
 }
